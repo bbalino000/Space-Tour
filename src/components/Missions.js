10997cab2686086dcdec0b3dac14fdff1d5f25e2
bbalino000 <balinobryan@gmail.com>
2023-08-07 10:20:35 +0800

    Last Changes

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,10 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 import styled from "styled-components";
-import bgMissions from '../images/space-photo.jpeg';
-import downChevron from '../images/down-chevron.svg';
-import { useRef, useState, useEffect } from 'react';
-import MissionFilter from './MissionFilter';
-import axios from 'axios';
+import bgMissions from "../images/space-photo.jpeg";
+import downChevron from "../images/down-chevron.svg";
+import { useRef, useState, useEffect } from "react";
+import MissionFilter from "./MissionFilter";
+import axios from "axios";
 
 const GlobalStyle = createGlobalStyle`
     body {
@@ -21,111 +21,110 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 
-const MainContainer = styled.div`
-`;
+const MainContainer = styled.div``;
 
 const InitContainer = styled.div`
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const HeaderText = styled.h2`
-    font-size: 64px;
-    text-align: center;
-    flex: 1;
-    display: flex;
-    align-items: center;
+  font-size: 64px;
+  text-align: center;
+  flex: 1;
+  display: flex;
+  align-items: center;
 `;
 
 const ChevronDown = styled.div`
-    height: 80px;
-    width: 40px;
-    margin-bottom: 20px;
-    background-color: var(--white);
-    mask: url(${downChevron}) no-repeat center;
-    -webkit-mask: url(${downChevron}) no-repeat center;
-    cursor: pointer;
+  height: 80px;
+  width: 40px;
+  margin-bottom: 20px;
+  background-color: var(--white);
+  mask: url(${downChevron}) no-repeat center;
+  -webkit-mask: url(${downChevron}) no-repeat center;
+  cursor: pointer;
 `;
 
 const Content = styled.div`
-    background: lightgray;
-    padding: 2%;
+  background: lightgray;
+  padding: 2%;
 `;
 
 const MissionsContainer = styled.div`
-    background: white;
+  background: white;
 `;
 
 const Copyright = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
 `;
 
 const CopyrightText = styled.div`
-    justify-self: flex-start;
-    color: gray;
+  justify-self: flex-start;
+  color: gray;
 `;
 
 const BackToTop = styled.div`
-    justify-self: flex-end;
-    cursor: pointer;
-    color: gray;
+  justify-self: flex-end;
+  cursor: pointer;
+  color: gray;
 `;
 
-function Missions () {
-    const [launches, setLaunches] = useState([]);
-    const [launchpads, setLaunchpads] = useState([]);
+function Missions() {
+  const [launches, setLaunches] = useState([]);
+  const [launchpads, setLaunchpads] = useState([]);
 
-    const top = useRef(null);
-    const content = useRef(null);
+  const top = useRef(null);
+  const content = useRef(null);
 
-    const handleScrollTo = (elementRef) => {
-        window.scrollTo({
-            top: elementRef.current.offsetTop,
-            behavior: 'smooth'
-        });
-    };
+  const handleScrollTo = (elementRef) => {
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
 
-    const fetchLaunches = async () => {
-        const res = await axios.get('http://localhost:8001/launches');
-        setLaunches(res.data);
-    };
+  const fetchLaunches = async () => {
+    const res = await axios.get("http://localhost:8001/launches");
+    setLaunches(res.data);
+  };
 
-    const fetchLaunchpads = async () => {
-        const res = await axios.get('http://localhost:8001/launchpads');
-        setLaunchpads(res.data);
-    };
-    
-    useEffect(() => {
-        fetchLaunchpads();
-        fetchLaunches();
-    }, []);
+  const fetchLaunchpads = async () => {
+    const res = await axios.get("http://localhost:8001/launchpads");
+    setLaunchpads(res.data);
+  };
 
-    return (
-        <MainContainer ref={top}>
-            <GlobalStyle />
-            
-            <InitContainer>
-                <HeaderText>DISCOVER SPACE MISSIONS</HeaderText>
-                <ChevronDown onClick={ () => handleScrollTo(content) } />
-            </InitContainer>
+  useEffect(() => {
+    fetchLaunchpads();
+    fetchLaunches();
+  }, []);
 
-            <Content ref={content}>
-                <MissionsContainer>
-                    <MissionFilter launches={launches} launchpads={launchpads}/>
-                </MissionsContainer>
+  return (
+    <MainContainer ref={top}>
+      <GlobalStyle />
 
-                <Copyright>
-                    <CopyrightText>Copyright &copy; 2023 Space Savvy</CopyrightText>
-                    <BackToTop onClick={ () => handleScrollTo(content) }>Back to top</BackToTop>
-                </Copyright>
-            </Content>
+      <InitContainer>
+        <HeaderText>DISCOVER SPACE MISSIONS</HeaderText>
+        <ChevronDown onClick={() => handleScrollTo(content)} />
+      </InitContainer>
 
-            
-        </MainContainer>
-    );
+      <Content ref={content}>
+        <MissionsContainer>
+          <MissionFilter launches={launches} launchpads={launchpads} />
+        </MissionsContainer>
+
+        <Copyright>
+          <CopyrightText>Copyright &copy; 2023 Space Savvy</CopyrightText>
+          <BackToTop onClick={() => handleScrollTo(content)}>
+            Back to top
+          </BackToTop>
+        </Copyright>
+      </Content>
+    </MainContainer>
+  );
 }
 
 export default Missions;
--- src/components/Technology.js
+++ src/components/Technology.js
@@ -2,10 +2,10 @@ import { useState, useRef, useEffect } from "react";
 import techData from "../data/technologies.json";
 import TechnologyDetails from "./TechnologyDetails";
 import styled from "styled-components";
-import { createGlobalStyle } from 'styled-components'
-import bgMobile from '../assets/technology/background-technology-mobile.jpg';
-import bgTablet from '../assets/technology/background-technology-tablet.jpg';
-import bgDesktop from '../assets/technology/background-technology-desktop.jpg';
+import { createGlobalStyle } from "styled-components";
+import bgMobile from "../assets/technology/background-technology-mobile.jpg";
+import bgTablet from "../assets/technology/background-technology-tablet.jpg";
+import bgDesktop from "../assets/technology/background-technology-desktop.jpg";
 
 const GlobalStyle = createGlobalStyle`
     @media only screen and (min-width: 320px) {
@@ -38,171 +38,175 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const MainContainer = styled.div`
-    @media only screen and (min-width: 992px) {
-        padding-top: 4%;
-        padding-left: 10%;
-        padding-bottom: 4%;
-    }
+  @media only screen and (min-width: 992px) {
+    padding-top: 4%;
+    padding-left: 10%;
+    padding-bottom: 4%;
+  }
 `;
 
 const FlexContainer = styled.div`
-    @media only screen and (min-width: 992px) {
-        display: flex;
-        flex-direction: row;
-        gap: 10%;
-        align-items: center;
-    }
+  @media only screen and (min-width: 992px) {
+    display: flex;
+    flex-direction: row;
+    gap: 10%;
+    align-items: center;
+  }
 `;
 
 const TechContainer = styled.div`
-    padding: 0 8%;
+  padding: 0 8%;
 
-    @media only screen and (min-width: 992px) {
-        display: flex;
-        flex-direction: row;
-        gap: 4em;
-        padding: 0;
-        order: 1;
-    }
+  @media only screen and (min-width: 992px) {
+    display: flex;
+    flex-direction: row;
+    gap: 4em;
+    padding: 0;
+    order: 1;
+  }
 `;
 
 const TechImg = styled.div`
-    background: url(${props => props.img}) no-repeat bottom;
-    background-size: cover;
-    width: 100%;
-    padding-top: 44%;
+  background: url(${(props) => props.img}) no-repeat bottom;
+  background-size: cover;
+  width: 100%;
+  padding-top: 44%;
 
-    @media only screen and (min-width: 992px) {
-        order: 2;
-        background-image: url(${ props => props.img.replace('landscape','portrait') });
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-position: center;
-        padding-block: 25%;
-    }
+  @media only screen and (min-width: 992px) {
+    order: 2;
+    background-image: url(${(props) =>
+      props.img.replace("landscape", "portrait")});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    padding-block: 25%;
+  }
 `;
 
 const NavText = styled.h5`
-    color: var(--white) !important;
-    margin: 0 auto;
-    text-align: center;
-    font-size: 16px;
-    margin-bottom: 2em;
+  color: var(--white) !important;
+  margin: 0 auto;
+  text-align: center;
+  font-size: 16px;
+  margin-bottom: 2em;
 
-    & > span {
-        color: var(--disabled);
-    }
+  & > span {
+    color: var(--disabled);
+  }
 
-    @media only screen and (min-width: 768px) {
-        text-align: left;
-        font-size: 18px;
-        padding: 4%
-    }
+  @media only screen and (min-width: 768px) {
+    text-align: left;
+    font-size: 18px;
+    padding: 4%;
+  }
 
-    @media only screen and (min-width: 992px) {
-        padding: 0;
-        margin: 0;
-        font-size: 22px;
-    }
+  @media only screen and (min-width: 992px) {
+    padding: 0;
+    margin: 0;
+    font-size: 22px;
+  }
 `;
 
 const Items = styled.div`
-    display: flex;
-    flex-direction: row;
-    gap: 1em;
-    justify-content: center;
-    margin: 2em 0;
+  display: flex;
+  flex-direction: row;
+  gap: 1em;
+  justify-content: center;
+  margin: 2em 0;
 
-    @media only screen and (min-width: 992px) {
-        display: flex;
-        flex-direction: column;
-        margin: 0;
-        justify-content: flex-start;
-    }
+  @media only screen and (min-width: 992px) {
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+    justify-content: flex-start;
+  }
 `;
 
 const Item = styled.span`
-    width: 44px; 
-    height: 44px;
-    border: 1px solid gray;
-    border-radius: 50%;
-    display: flex;
-    align-items: center; 
-    justify-content: center;
-    font-size: 16px;
-    font-family: Bellefair;
-    cursor: pointer;
+  width: 44px;
+  height: 44px;
+  border: 1px solid gray;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  font-family: Bellefair;
+  cursor: pointer;
 
-    &.active {
-        background-color: var(--white);
-        color: var(--black);
-    }
+  &.active {
+    background-color: var(--white);
+    color: var(--black);
+  }
 
-    @media only screen and (min-width: 768px) {
-        width: 54px; 
-        height: 54px;
-    }
+  @media only screen and (min-width: 768px) {
+    width: 54px;
+    height: 54px;
+  }
 
-    @media only screen and (min-width: 992px) {
-        width: 70px; 
-        height: 70px;
-        font-size: 26px;
-    }
+  @media only screen and (min-width: 992px) {
+    width: 70px;
+    height: 70px;
+    font-size: 26px;
+  }
 `;
 
-function Technology () {
-    const [index, setIndex] = useState(0);
-    const timerRef = useRef(null);
+function Technology() {
+  const [index, setIndex] = useState(0);
+  const timerRef = useRef(null);
 
-    function resetTimeout() {
-        if (timerRef.current) {
-            clearTimeout(timerRef.current);
-        }
+  function resetTimeout() {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
     }
+  }
 
-    useEffect(() => {
-        resetTimeout();
+  useEffect(() => {
+    resetTimeout();
 
-        timerRef.current = setTimeout(() => {
-            setIndex((prevIndex) => (prevIndex === techData.length - 1) ? 0 : prevIndex + 1);
-        }, 10000);
-      
-        return () => {
-          resetTimeout();
-        };
+    timerRef.current = setTimeout(() => {
+      setIndex((prevIndex) =>
+        prevIndex === techData.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 10000);
 
-    }, [index]);
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
 
-    const items = techData.map((data, idx) => {
-        return (
-            <Item className={ index === idx ? 'active' : '' }
-                  key = {idx}
-                  onClick= { () => {
-                    setIndex(idx);
-                  }}>
-                { idx + 1 }
-            </Item>
-        );
-    });
-
+  const items = techData.map((data, idx) => {
     return (
-        <MainContainer>
-            <GlobalStyle />
-            <NavText><span>03</span> SPACE LAUNCH 101</NavText>
-
-            <FlexContainer>
-                <TechImg img={ techData[index].imgPath } />
-                
-                <TechContainer>
-                    <Items>
-                        {items}
-                    </Items>
-
-                    <TechnologyDetails data={techData[index]} />
-                </TechContainer>
-            </FlexContainer>
-        </MainContainer>
+      <Item
+        className={index === idx ? "active" : ""}
+        key={idx}
+        onClick={() => {
+          setIndex(idx);
+        }}
+      >
+        {idx + 1}
+      </Item>
     );
+  });
+
+  return (
+    <MainContainer>
+      <GlobalStyle />
+      <NavText>
+        <span>03</span> SPACE LAUNCH 101
+      </NavText>
+
+      <FlexContainer>
+        <TechImg img={techData[index].imgPath} />
+
+        <TechContainer>
+          <Items>{items}</Items>
+
+          <TechnologyDetails data={techData[index]} />
+        </TechContainer>
+      </FlexContainer>
+    </MainContainer>
+  );
 }
 
 export default Technology;
